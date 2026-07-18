1afeba5d1c4723edce3e455185416d8be826d7a4
Blockyboy <theblockyguy4@gmail.com>
2026-07-18 14:28:10 +0930

    Add project files.

---
+++ RosteringApplication/Model/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RosteringApplication.Model
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Role { get; set; }
+
+        public Employee(int id, string firstName, string lastName, string role)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Role = role;
+        }
+
+    }
+}
---
+++ RosteringApplication/ViewModel/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RosteringApplication.Model;
+
+namespace RosteringApplication.ViewModel
+{
+    public class MainViewModel
+    {
+        public ObservableCollection<Employee> Employees { get; set; }
+
+        public MainViewModel()
+        {
+            Employees = [ new Employee(1, "Ivan", "Pribster", "Worker") ];
+        }
+    }
+}
